53a192d517636c1a4fff86fa4e7b768d5d0c5349
Tanoosree2006 <147473260+Tanoosree2006@users.noreply.github.com>
2025-12-16 15:32:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
+        stack<char>st;
+        for(char ch:s){
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+                char top=st.top();
+                st.pop();
+                if(ch==')' && top!='(' || ch=='}' && top!='{' || ch==']' &&top!='['){
+                    return false;
+                }
+            }
+        }
+            return st.empty();
+    }
+};
